eb30a76088eab2ea954bbbe921113b31cebcf6ee
rJ0h1 <rabia.joachimsmann@sogeti.com>
2024-10-14 13:11:13 +0200

    Prep code for Exercise-3

--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -2,22 +2,13 @@ package StepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.restassured.RestAssured;
-import services.ConfigurationService;
 
 public class LoginStepDefinitions {
 
-    private final ConfigurationService service;
+    @Given("user wants to access the wizard api")
+    public void userWantsToAccessTheWizardWorldApi() {
 
-    public LoginStepDefinitions(ConfigurationService configurationService) {
-
-        this.service = configurationService;
-    }
-    @Given("user wants to access the {string} api")
-    public void userWantsToAccessTheWizardWorldApi(String apiName) {
-
-        service.initializeApis();
-        RestAssured.baseURI = service.getApis().getApi(apiName).getScheme() + "://"
-                + service.getApis().getApi(apiName).getUri();
+        RestAssured.baseURI = "http://wizard-world-api.herokuapp.com";
 
     }
 }
--- src/test/resources/features/wizard.feature
+++ src/test/resources/features/wizard.feature
@@ -1,7 +1,7 @@
 Feature: feature file that tests WizardWorldApi
 
    Background: user is testing the wizerd-world-api
-     Given user wants to access the "wizard" api
+     Given user wants to access the wizard api
 
   Scenario: retrieve list of all elixirs and validate response
   Given that user wants to review the list of Elixirs
