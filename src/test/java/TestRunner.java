8a3a076b4fa8cfd03c5ed96c77861b922be253bd
rJ0h1 <rabia.joachimsmann@sogeti.com>
2024-10-14 11:48:08 +0200

    Cleaned up branch for 1st exercise

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,11 +1,8 @@
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/wizard.feature")
-@ConfigurationParameter(key = "cucumber.glue", value = "StepDefinitions")
 @ConfigurationParameter(key = "cucumber.plugin", value = "html:target/cucumber.html")
 public class TestRunner {
 }
